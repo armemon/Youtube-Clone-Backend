d1f7dbabebfd140b04a626e6cd1c26c37048b76e
Abdul Rahman Memon <abdul.rehman@markematicsdigital.com>
2024-01-29 20:26:07 +0500

    Made Rotes for user

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 // body parser is default imported in new Node else we have to install and import
 
-app.use(express.urlencoded({extented: true, limit:"16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
 // extended true allow nested objects in url
 
 app.use(express.static("public"))
@@ -22,5 +22,17 @@ app.use(cookieParser())
 
 // app.use(middleware)
 // use is used when we are using middleware or doing any configuartion
+
+
+
+// routes import  // file segeration ( routes are imported after configuartion)
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+// app.get     // will not be used as we have seperated things
+// to use router you need middleware
+app.use("/api/v1/users", userRouter)
+// users is prefix whenver localhost8000/api/v1/users it will go in userRouter
+
 
 export { app }
