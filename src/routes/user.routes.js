e234966d81f7d4d1b4ccd27e43b31d34b07a3ea0
Abdul Rahman Memon <abdul.rehman@markematicsdigital.com>
2024-01-31 20:23:45 +0500

    small changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar"
+        }
+
+    ]),
+    registerUser)
 
 export default router
