3c3048ced27aa44edfc2ce3b78a57a681df92e17
armemon <arahman.ar2820@gmail.com>
2024-02-20 16:55:14 +0500

    Added subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
